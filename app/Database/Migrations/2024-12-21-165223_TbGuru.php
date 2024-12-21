0f6a8e1b98c330dc6d036bd6f1430ad719ef435a
saepulfariz <saepulfariz108@gmail.com>
2024-12-21 23:55:37 +0700

    add migration tb_guru

---
+++ app/Database/Migrations/2024-12-21-165223_TbGuru.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class TbGuru extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'nik' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'jk' => [
+                'type'       => 'CHAR',
+                'constraint' => '1',
+            ],
+            'email' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'no_hp' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('tb_guru');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('tb_guru');
+    }
+}
