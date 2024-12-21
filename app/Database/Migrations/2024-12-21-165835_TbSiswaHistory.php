1ddf97f24ca3bc6507142c83c1123ccd4f0aeaa3
saepulfariz <saepulfariz108@gmail.com>
2024-12-21 23:59:55 +0700

    add table siswa history

---
+++ app/Database/Migrations/2024-12-21-165835_TbSiswaHistory.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class TbSiswaHistory extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'id_siswa' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+            ],
+            'id_kelas' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+            ],
+            'id_jurusan' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+            ],
+            'id_tahun' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->addForeignKey('id_siswa', 'tb_siswa', 'id');
+        $this->forge->createTable('tb_siswa_history');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('tb_siswa_history');
+    }
+}
