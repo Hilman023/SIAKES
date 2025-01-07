bc6868a90fe4207720a7fd4c4c1ad50f35fd59e0
saepulfariz <saepulfariz108@gmail.com>
2025-01-07 21:57:22 +0700

    add library datatables

--- app/Views/template/header.php
+++ app/Views/template/header.php
@@ -27,6 +27,10 @@
 
   <link rel="stylesheet" href="<?= base_url(); ?>public/assets/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
 
+  <link rel="stylesheet" href="<?= base_url(); ?>public/assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="<?= base_url(); ?>public/assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+  <link rel="stylesheet" href="<?= base_url(); ?>public/assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+
   <?= $this->renderSection('head'); ?>
 
   <style>
